7c66016df0bb7db351e1c0d2a72c737f9b9f9d8d
Akiyama Yoh <gutugutu3030@gmail.com>
2016-12-24 01:20:49 +0900

    pre 8mm

--- contents/8mmFilm/sketch.js
+++ contents/8mmFilm/sketch.js
@@ -1,31 +1,34 @@
-var title,explain;
+var title, explain;
 var w = 100;
 
 var backC;
 
 var index = 0;
 var imgs;
-var upImage,downImage;
+var upImage, downImage;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight-80);
-  title = createElement('span', '<h1>８ミリフィルムプロジェクト</h1><p>インクジェットプリンタやレーザカッターで，8mmフィルム自体を自作（キネコ）しています．そのフィルムを用いた作品集です．</p>');
+  createCanvas(windowWidth, windowHeight - 80);
+  title = createElement('span', '<div><h1>８ミリフィルムプロジェクト</h1><p>インクジェットプリンタやレーザカッターで，8mmフィルム自体を自作（キネコ）しています．そのフィルムを用いた作品集です．</p></div><div><h3 id="imgTitle"></h3><p id="imgContent"></p></div>');
+  explain = createElement('span', '');
   fixTextPosition();
   backC = color(239, 173, 7);
   w = height * 2 / 5;
-  upImage=loadImage("./up.png");
-  downImage=loadImage("down.png");
+  upImage = loadImage("./up.png");
+  downImage = loadImage("down.png");
   loadImages();
+  updateExplain();
   textSize(60);
-  textAlign(CENTER,CENTER);
+  textAlign(CENTER, CENTER);
 }
 
-function loadImages(){
-  imgs=Array();
-  var i=0;
-  var imgTitle=["BadApple!! in 8mm Film"];
-  var imgTitle=["BadApple!! in 8mm Film"];
-  imgs[i++]={img:loadImage("badapple.png"),title:"BadApple!! in 8mm Film",contant:"Bad Apple!! 影絵を8mm映写機で再生"};
+function loadImages() {
+  imgs = Array();
+  var i = 0;
+  imgs[i++] = { img: loadImage("badapple/thumbnail.jpg"), title: "BadApple!! 影絵", content: "初めての長編自作フィルムです．白黒．作り方の詳細などは<a href='badapple/index.html'>こちら</a>",link:"http://www.nicovideo.jp/watch/sm28787820" };
+  imgs[i++] = { img: loadImage("sha.png"), title: "舎利禮文", content: "初のカラーフィルムです．" ,link:"http://www.nicovideo.jp/watch/sm28845675"};
+  imgs[i++] = { img: loadImage("ROD.png"), title: "R.O.D -The TV- OP", content: "カラーの鮮やかなフィルムです．",link:"http://www.nicovideo.jp/watch/sm28869077" };
+  imgs[i++] = { img: loadImage("film.png"), title: "自作フィルムのつくりかた", content: "フィルム制作の解説動画です．",link:"http://www.nicovideo.jp/watch/sm28876844" };
 }
 
 
@@ -49,10 +52,11 @@ function drawButton() {
   stroke(0);
   fill(52, 202, 255);
   rect(x, y, w, h);
-  fill(255,sin((onPlayButton?0.12:0.06)*frameCount)*80+80);
+  fill(255, sin((onPlayButton ? 0.12 : 0.06) * frameCount) * 80 + 80);
   rect(x, y, w, h);
   fill(255);
-  text("play!",x+w/2,y+h/2);
+  text("play!", x + w / 2, y + h / 2);
+  //console.log(movingTime);
 }
 
 function drawFilm() {
@@ -63,16 +67,34 @@ function drawFilm() {
   fill(255);
   if (movingTime == 10) {
     movingTime = 0;
+    index = (index + moving + imgs.length) % imgs.length;
+    updateExplain();
     moving = 0;
   }
   for (var i = 0; i < 7; i++) {
     fill(backC);
     rect(15, 10 + h * i + h / 2 - w / 20 - moving * h * movingTime / 10 - h, w / 15, w / 10);
+    if(moving==0&&i==3){
+      fill(255,sin(0.04 * frameCount) * 75+150,sin(0.04 * frameCount) * 75+150);
+      rect(35 + w / 15, 5 + h * i - moving * h * movingTime / 10 - h, w - 40 - w / 15, h);
+    }
     fill(255);
     rect(40 + w / 15, 10 + h * i - moving * h * movingTime / 10 - h, w - 50 - w / 15, h - 10);
-    if(i==1){
-      image(upImage,40 + w / 15, 10 + h * i - moving * h * movingTime / 10 - h, w - 50 - w / 15, h - 10);
+    var tint1 = 255;
+    //console.log(imgs);
+    if (2 <= i && i <= 4) {
+      //console.log(i+" "+((imgs.length+index+i-3)%imgs.length));
+      image(imgs[(imgs.length + index + i - 3) % imgs.length].img, 40 + w / 15, 10 + h * i - moving * h * movingTime / 10 - h, w - 50 - w / 15, h - 10);
     }
+    if (i == ((moving == -1) ? 0 : 1)) {
+      image(upImage, 40 + w / 15, 10 + h * i - moving * h * movingTime / 10 - h, w - 50 - w / 15, h - 10);
+    }
+    if (i == ((moving == 1) ? 6 : 5)) {
+      image(downImage, 40 + w / 15, 10 + h * i - moving * h * movingTime / 10 - h, w - 50 - w / 15, h - 10);
+    }
+  }
+  if (movingTime != 0) {
+    movingTime++;
   }
 }
 
@@ -86,7 +108,7 @@ function keyPressed() {
   if (keyCode === DOWN_ARROW) {
     moving = 1;
   }
-  if(keyCode===ENTER){
+  if (keyCode === ENTER) {
     play();
     return;
   }
@@ -94,7 +116,22 @@ function keyPressed() {
     movingTime = 1;
   }
 }
-
+var scroll=0;
+function mouseWheel(event) {
+  if(event.delta<-2){
+    moving=-1;
+  }
+  if(2<event.delta){
+    moving=1;
+  }
+  if (moving != 0 && movingTime == 0) {
+    movingTime = 1;
+  }
+}
+function updateExplain() {
+  document.getElementById("imgTitle").innerHTML = imgs[index].title;
+  document.getElementById("imgContent").innerHTML = imgs[index].content;
+}
 function mouseMoved() {
   var w = 200;
   var h = w / 2.5;
@@ -102,23 +139,25 @@ function mouseMoved() {
   var y = height - h - 50;
   onPlayButton = (x <= mouseX && mouseX <= x + w && y <= mouseY && mouseY <= y + h);
 }
-function mouseReleased(){
-  if(onPlayButton){
+function mouseReleased() {
+  if (onPlayButton) {
     play();
   }
 }
-function play(){
+function play() {
   print("play");
+  window.open(imgs[index].link, '_blank');
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight-80);
+  resizeCanvas(windowWidth, windowHeight - 80);
   w = height * 2 / 5; //min(width/2,height/2);
   fixTextPosition();
 
 }
 
-function fixTextPosition(){
-  title.position(width/2, 80);
+function fixTextPosition() {
+  title.position(width / 2, 80);
+
   //explain.position(width/2, 50);
 }
